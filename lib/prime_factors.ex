07e24846f6cf7ebc5a4fe1a274dc1d21cd913f21
Koll Johnson <kolljohnson@Kolls-MacBook-Pro.local>
2017-06-30 13:41:35 -0400

    Refactor

--- lib/prime_factors.ex
+++ lib/prime_factors.ex
@@ -3,20 +3,23 @@ defmodule PrimeFactors do
   Documentation for PrimeFactors.
   """
 
-def factorize (number) do
-    is_prime(number, 2, [])
-end
-  
-def is_prime(number,divisor, apples ) do
-    if number != 1 do
-      if (rem(number, divisor) == 0) do
-	is_prime(trunc(number / divisor), divisor, apples ++ [divisor])
-      else
-	is_prime(number, divisor + 1, apples)
-      end
-    else 
-      apples
+def factorize(number), do: factorize(number, 2, [])  
+def factorize(number,divisor, apples) do
+  if number != 1 do
+    cond do
+      is_divisible?(number, divisor) ->
+	next_number = trunc(number / divisor) 
+	factorize(next_number, divisor, apples ++ [divisor])
+	
+      true ->
+	factorize(number, divisor + 1, apples)
     end
+  else 
+      apples
+  end
 end
+
+defp is_divisible?(number, divisor), do: rem(number, divisor) == 0
+
 end
 
